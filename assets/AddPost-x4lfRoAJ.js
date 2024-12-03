211b4cc1dd54a18027f6dded14a4c628b0765d9e
hero <ronakthapa37@gmail.com>
2024-12-03 06:02:01 +0545

    fixes 2

---
+++ assets/AddPost-x4lfRoAJ.js
@@ -0,0 +1,1 @@
+import{j as r,C as o}from"./index-B_LjZI-9.js";import{P as s}from"./Post_Form-COmtLXB7.js";import"./Sub_Loader-DZZJcQpC.js";const m=()=>r.jsx("div",{children:r.jsx(o,{children:r.jsx(s,{})})});export{m as default};
--- assets/AddPost-xZBNAsEr.js
+++
@@ -1,1 +0,0 @@
-import{j as r,C as o}from"./index-DDIWK_Zb.js";import{P as s}from"./Post_Form-KfA9Wf00.js";import"./Sub_Loader-CVCoM0at.js";const m=()=>r.jsx("div",{children:r.jsx(o,{children:r.jsx(s,{})})});export{m as default};
